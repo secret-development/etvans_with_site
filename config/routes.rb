2070b71000a706fb7a4fdd0be27810da64c229c3
Yerlan Kussainov <lagox.mail@gmail.com>
2012-06-24 14:07:12 +0600

    routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,79 +1,85 @@
 # -*- encoding : utf-8 -*-
 Crm::Application.routes.draw do
 
-  root :to => 'results#index'
+  # root :to => 'results#index'
 
-  resources :customeraccesses
+  scope "/etvans" do
+    match "search" => 'results#index'
+    
+    resources :customeraccesses
 
-  resources :paginators, :except => [:new, :create, :destroy]
-  resources :worktimes
+    resources :paginators, :except => [:new, :create, :destroy]
+    resources :worktimes
 
-  get "reports" => "reports#index", :as => :reports
-  resource :reports do
-    get "activ", :on => :member, :as => :activ
-    get "finance", :on => :member, :as => :finance
-    get "general", :on => :member, :as => :general
-  end
-  get "sign_in" => "sessions#new", :as => "sign_in"
-  get "sign_out" => "sessions#destroy", :as => "sign_out"
+    get "reports" => "reports#index", :as => :reports
+    resource :reports do
+      get "activ", :on => :member, :as => :activ
+      get "finance", :on => :member, :as => :finance
+      get "general", :on => :member, :as => :general
+    end
+    get "sign_in" => "sessions#new", :as => "sign_in"
+    get "sign_out" => "sessions#destroy", :as => "sign_out"
 
-  get "sign_up => " "users/new", :as => "sign_up"
-  resources :users do
-    get 'add_info', :on => :member, :as => :add_info
-  end
-  resource :sessions
-  resource :password_resets
+    get "sign_up => " "users/new", :as => "sign_up"
+    resources :users do
+      get 'add_info', :on => :member, :as => :add_info
+    end
+    resource :sessions
+    resource :password_resets
 
-  get "potentials/index"
-  match "potentials" => "potentials#index", :as => :potentials
-  resources :tasks do
-    get 'admin', :on => :collection
-  end
-  resources :customers do
-    get 'all', :on => :collection
-    post 'lastcallcustomer', :on => :collection
-  end
-  
-  resources :subjects do
-    resources :photos, :only => [:create, :destroy]
-    get 'add_properties', :on => :member, :as => :add_properties
-    get 'add_photo', :on => :member, :as => :add_photo
-    get 'guest', :on => :member, :as => :guest
-    post 'findtypesubject', :on => :collection
-    post 'load_attr', :on => :collection
-    # scopes:
-    get 'active', :on => :collection
-    get 'inactive', :on => :collection
+    get "potentials/index"
+    match "potentials" => "potentials#index", :as => :potentials
+    resources :tasks do
+      get 'admin', :on => :collection
+    end
+    resources :customers do
+      get 'all', :on => :collection
+      post 'lastcallcustomer', :on => :collection
+    end
+
+    resources :subjects do
+      resources :photos, :only => [:create, :destroy]
+      get 'add_properties', :on => :member, :as => :add_properties
+      get 'add_photo', :on => :member, :as => :add_photo
+      get 'guest', :on => :member, :as => :guest
+      post 'findtypesubject', :on => :collection
+      post 'load_attr', :on => :collection
+      # scopes:
+      get 'active', :on => :collection
+      get 'inactive', :on => :collection
+
+    end
+
+    resources :transactions do
+      get 'all', :on => :collection
+      resources :documents, :only => [:create, :destroy]
+      get 'add_document', :on => :member, :as => :add_document
+    end
+    resources :cities, :except => [:show]
+    resources :social_statuses, :except => [:show]
+    resources :condition_fields
+    resources :value_fields, :except => [:show]
+    resources :typesubjects, :except => [:show]
+    resources :districts, :except => [:show]
+    resources :residents, :except => [:show]
+    # settings
+    match "settings" => "settings#index", :as => :settings
+
+    # for search
+    resources :results
+
+    # help:
+    get "help/search" => "help#search", :as => :help_search
+    get "help/customers" => "help#customers", :as => :help_customers
+    get "help/subjects" => "help#subjects", :as => :help_subjects
+    get "help/users" => "help#users", :as => :help_users
+    get "help/tasks" => "help#tasks", :as => :help_tasks
+    get "help/transactions" => "help#transactions", :as => :help_transactions
+    get "help/reports" => "help#reports", :as => :help_reports
+    get "help/danger" => "help#danger", :as => :help_danger
     
   end
-  
-  resources :transactions do
-    get 'all', :on => :collection
-    resources :documents, :only => [:create, :destroy]
-    get 'add_document', :on => :member, :as => :add_document
-  end
-  resources :cities, :except => [:show]
-  resources :social_statuses, :except => [:show]
-  resources :condition_fields
-  resources :value_fields, :except => [:show]
-  resources :typesubjects, :except => [:show]
-  resources :districts, :except => [:show]
-  resources :residents, :except => [:show]
-  # settings
-  match "settings" => "settings#index", :as => :settings
-  
-  # for search
-  resources :results
-  
-  # help:
-  get "help/search" => "help#search", :as => :help_search
-  get "help/customers" => "help#customers", :as => :help_customers
-  get "help/subjects" => "help#subjects", :as => :help_subjects
-  get "help/users" => "help#users", :as => :help_users
-  get "help/tasks" => "help#tasks", :as => :help_tasks
-  get "help/transactions" => "help#transactions", :as => :help_transactions
-  get "help/reports" => "help#reports", :as => :help_reports
-  get "help/danger" => "help#danger", :as => :help_danger
+
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
